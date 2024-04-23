71a195bc8de32a4411dbfb395e5bcac20824e959
Jaroslav Krc <122451815+617263686974656374@users.noreply.github.com>
2024-04-23 16:51:37 +0200

    Add files via upload

---
+++ KMS1_05_LE_09_02/Vereinsverwaltung/Player.java
@@ -0,0 +1,18 @@
+public class Player {
+    private String namePlayer;
+    private String position;
+
+    public Player(String namePlayer, String position) {
+        this.namePlayer = namePlayer;
+        this.position = position;
+    }
+
+
+    public String getNamePlayer() {
+        return namePlayer;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+}
---
+++ KMS1_05_LE_09_02/Vereinsverwaltung/Trainer.java
@@ -0,0 +1,11 @@
+public class Trainer {
+    private String nameTrainer;
+
+    public Trainer(String nameTrainer) {
+        this.nameTrainer = nameTrainer;
+    }
+
+    public String getNameTrainer() {
+        return nameTrainer;
+    }
+}
